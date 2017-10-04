160c3fc00c7f7547b7fb4ef59a08a3c7b19d938f
Chris Marshall <cmarshall10450@gmail.com>
2017-10-04 11:27:07 +0100

    Dice - test_cam_roll_random_number passing

--- Dice.rb
+++ Dice.rb
@@ -3,7 +3,11 @@ class Dice
 	attr_reader :sides
 
 	def initialize()
-		@sides = 6
+		@sides = [1,2,3,4,5,6]
+	end
+
+	def roll
+		return @sides.sample()
 	end
 
 end
--- specs/Dice.spec.rb
+++ specs/Dice.spec.rb
@@ -8,12 +8,14 @@ class TestDice < MiniTest::Test
 	end
 
 	def test_has_6_sides
-		assert_equal(6, @dice.sides)
+		assert_equal(6, @dice.sides.count)
 	end
 
-	# def test_dice_rolls_random_number_between_1_and_6
-	# 	result = @dice.roll
-	# 	assert_equal((1..6), result)
-	# end
+	def test_dice_rolls_random_number_between_1_and_6
+		result = @dice.roll
+		possible_nums = [1, 2, 3, 4, 5, 6]
+
+		assert_equal(true, possible_nums.include?(result))
+	end
 
 end
